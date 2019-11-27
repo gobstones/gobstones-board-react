0fc1ec24841f1b27e20dda7b066cedb254a6aadd
Luca <lhazuca@gmail.com>
2019-11-27 10:04:13 -0300

    Creo tablero de BOOM.

---
+++ src/components/BoomBoard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function BoomBoard() {
+    return <div className={"gbs-boom-container"}>
+        <img
+            src={"https://user-images.githubusercontent.com/1631752/37945593-54b482c0-3157-11e8-9f32-bd25d7bf901b.png"} alt={"exploded board"}/>
+        <h2>BOOM</h2>
+        <p className={"gbs-boom-info"}>Aca va el error</p>
+    </div>;
+}
