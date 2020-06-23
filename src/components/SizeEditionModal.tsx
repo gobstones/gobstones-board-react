3827ec8d39fa0c8ec6b98f82be33f6b37c9ffd9e
Luca <lhazuca@gmail.com>
2020-06-23 20:56:44 -0300

    Estilos + boton random

--- src/components/SizeEditionModal.tsx
+++ src/components/SizeEditionModal.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
 import '../style/size-edition.css'
 import {Coord} from "./CellManager";
+import dice from '../img/dice.svg'
+import {randomInt} from "../utils/random";
 
 interface Props {
     initialRows: number;
@@ -11,15 +13,17 @@ interface Props {
     headSetter: (coord: Coord) => void;
 }
 
-function NumericInput(props: {label: string, value: number, onChange: (e: number) => void }) {
+function NumericInput(props: { label: string, value: number, onChange: (e: number) => void }) {
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         const number = parseInt(e.target.value.replace(/\D/g, ''));
         props.onChange(isNaN(number) ? 0 : number);
     }
 
     return <div className='input_container'>
-        <label>{props.label} </label>
-        <input value={props.value} onChange={(e) => handleChange(e)}/>
+        <label className='modal_label'>{props.label} </label>
+        <input className='modal_input' value={props.value} onChange={(e) => handleChange(e)}/>
+        <img src={dice} alt='Random' className='random_button'
+             onClick={() => props.onChange(randomInt(1,100))}/>
     </div>;
 }
 
@@ -45,22 +49,25 @@ export const SizeEditionModal = ({rowQuantitySetter, columnQuantitySetter, headS
         <div className={`modal ${show && 'active'}`}>
             <div className='modal_section'>
                 <label className='section_title'>Board size</label>
-                <NumericInput label='Columns' value={x} onChange={setX}/>
-                <NumericInput label='Rows' value={y} onChange={setY}/>
+                <div className='modal_section_break'></div>
+                <NumericInput label='Columns: ' value={x} onChange={setX}/>
+                <NumericInput label='Rows: ' value={y} onChange={setY}/>
             </div>
             <div className='modal_section'>
                 <label className='section_title'>Head position</label>
-                <NumericInput label='At column' value={headX} onChange={setHeadX}/>
-                <NumericInput label='At rows' value={headY} onChange={setHeadY}/>
+                <div className='modal_section_break'></div>
+                <NumericInput label='At column: ' value={headX} onChange={setHeadX}/>
+                <NumericInput label='At rows: ' value={headY} onChange={setHeadY}/>
             </div>
             <div className='modal_section'>
-                <button onClick={() => {
+                <button className='modal_button' onClick={() => {
                     columnQuantitySetter(5);
                     rowQuantitySetter(5);
-                    headSetter({x:0, y:0})
-                }}>New Board</button>
+                    headSetter({x: 0, y: 0})
+                }}>New Board
+                </button>
             </div>
-            <button onClick={() => {
+            <button className='modal_button' onClick={() => {
                 columnQuantitySetter(y);
                 rowQuantitySetter(x);
                 headSetter({x: headX, y: headY})
