e15e0b7c279e1d8243f9c2a3f3ad6f95984faaf6
Nicolas Alvarez <nicolas.alv3@gmail.com>
2021-01-23 16:49:00 -0300

    Arregla criterio de match

--- src/components/Attire.ts
+++ src/components/Attire.ts
@@ -1,13 +1,18 @@
-import { CellContent } from "./Cell";
-
 export type AttireJSON = {
     enabled : boolean,
     rules : Rule[],
     borders : Borders,
 }
 
+export type CellWhenAttire = {
+    red: number | string;
+    green:number | string;
+    black:number | string;
+    blue:number | string;
+}
+
 export type Rule = {
-    when : CellContent,
+    when : CellWhenAttire,
     image : string,
     text?:string
 }
@@ -48,11 +53,11 @@ export default class Attire {
         return this.attire;
     }
 
-    getAttireFor(black : number | string,blue : number | string,green :number | string,red:number | string){
+    getAttireFor(black : number, blue : number, green :number, red:number) {
         return this.getwhen(black,blue,green,red)
     }
 
-    getwhen(black : number | string,blue : number | string,green :number | string,red:number | string){
+    getwhen(black : number, blue : number, green :number, red:number) {
         const rule = this.attire.rules.find( rule => this.ruleMatchWith(rule,black,blue,green,red))
         if (!rule){
             return {image:"", text:""}
@@ -60,15 +65,24 @@ export default class Attire {
         return { image:rule.image,text:rule.text || "" }
     }
 
-    ruleMatchWith(rule : Rule,black : number | string,blue : number | string,green :number | string,red:number | string){
+    ruleMatchWith(rule : Rule,black : number, blue : number, green :number, red:number) {
         return this.match(rule.when.black, black) 
             && this.match(rule.when.blue, blue) 
             && this.match(rule.when.green, green) 
             && this.match(rule.when.red, red)
     }
 
-    match(when : number | string, amount : number | string){
-        return when === amount || (when === "*" && amount > 99)
+    match(when : number | string,  amount : number) {
+        return typeof when === 'number' ? when === amount : this.matchRegex(when as string, amount)
+    }
+
+    matchRegex(when :string, amount : number) {
+        switch (when) {
+            case "*" : return amount > 0;
+            case "+" : return amount >= 0;
+            default : return false;
+        }
+
     }
 
     getTopLeftCorner(){
